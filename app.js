b55c0174afc5730f5ae33fa7f631aa5a66ac4ba6
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2025-04-02 04:22:18 +0530

    Connection established with socket.io

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express")
+const socket = require("socket.io")
+const http = require("http");
+const path = require("path")
+const { Chess } = require("chess.js")
+
 const app = express()
 
+const server = http.createServer(app)
+const io = socket(server)
+const chess = new Chess()
+let players = {}
+let currentPlayer = "w"
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get("/", (req, res) => {
-    res.send("Hey")
+    res.render("index", {title: "Chess"})
 })
 
-app.listen(3000)
+io.on("connection", (uniquesocket) => {
+    console.log("connected")
+    
+})
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
